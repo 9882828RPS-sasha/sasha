9c0f0965bbf9a678899793fca69d535375c2fa84
Traun1 <kapitonovdmitrij5@gmail.com>
2024-02-28 13:41:27 +0300

    Задание готово к проверке

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,1 +1,11 @@
-﻿
+﻿#include <stdio.h>
+#include <math.h>
+int main()
+{
+	double x, y;
+	printf("Ведите значения x:");
+	scanf_s("%f", &x);
+	y = pow(x, 3) + pow(x, 2) - 10) / 2;
+	printf("результат: y= %f\n;y");
+	return 0;
+}
